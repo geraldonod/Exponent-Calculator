eb21a63fc79968549fe2c8c9f65bfe2479d370dd
Gerald <geraldonod@gmail.com>
2024-01-26 19:31:43 +0800

    create loop

--- Exponent Calculator.py
+++ Exponent Calculator.py
@@ -4,5 +4,9 @@ def exponent(base, exp):
     result = 1
 
 #create loop to get exponent
+    while num > 0:
+        result = result * base
+        num = num - 1
+    print(f"{base} raises to the power of {exp} is: {result}")
 
 #print result
